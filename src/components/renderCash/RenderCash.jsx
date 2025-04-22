f0c415fe44def78c26f83de50c3eea4a6830efe4
Nguyễn Mạnh Tùng <63092029+FPT-NMTung@users.noreply.github.com>
2025-04-22 09:10:02 +0700

    Update

--- src/components/renderCash/RenderCash.jsx
+++ src/components/renderCash/RenderCash.jsx
@@ -1,6 +1,7 @@
 import {useDispatch, useSelector} from "react-redux";
 import {formatNumber} from "../../common/common.js";
-import {change} from '../../redux/slice/hideAmount.js'
+import {change} from '../../redux/slice/hideAmount.js';
+import {useState} from "react"
 
 const RenderCash = (props) => {
     const hideAmount = useSelector((state) => state.hideAmount.value)
